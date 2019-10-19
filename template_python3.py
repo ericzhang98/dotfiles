2a248009e44957269bf90cfc339f1fcc731a399f
Eric Zhang <zhangeric99@gmail.com>
2019-10-19 13:12:24 -0700

    icpc

---
+++ template_python3.py
@@ -0,0 +1,22 @@
+import sys
+import math
+from collections import defaultdict, Counter
+import heapq
+import itertools
+import copy
+
+def solution(args):
+    return 0
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+params = []
+while True:
+    try:
+        line = input()
+    except:
+        break
+    params.append(line)
+
+solution(params)
